bf45be2ea6b4860d0a50e4608327bd945335406a
liutf <liutf@ndky.com>
2022-01-21 13:56:09 +0800

    forth commit

--- src/main/java/net/ltf/git/TestGit.java
+++ src/main/java/net/ltf/git/TestGit.java
@@ -6,6 +6,7 @@ public class TestGit {
         System.out.println("hello,word1");
         System.out.println("hello,word2");
         System.out.println("hello,word3");
+        System.out.println("hot-fix change");
     }
 
 }
