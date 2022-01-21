d06f499a384ecb72b11c3fda3c977315a5a8d42c
liutf <liutf@ndky.com>
2022-01-21 13:41:10 +0800

    third commit

--- src/main/java/net/ltf/git/TestGit.java
+++ src/main/java/net/ltf/git/TestGit.java
@@ -5,6 +5,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello,word1");
         System.out.println("hello,word2");
+        System.out.println("hello,word3");
     }
 
 }
