8c40e947378bbeb45922244b9de5d0a65d2ac1d4
liutf <liutf@ndky.com>
2022-01-21 14:04:48 +0800

    master commit

--- src/main/java/net/ltf/git/TestGit.java
+++ src/main/java/net/ltf/git/TestGit.java
@@ -8,6 +8,7 @@ public class TestGit {
         System.out.println("hello,word3");
         System.out.println("hot-fix change");
         System.out.println("master test merge");
+        System.out.println("master test merge");
     }
 
 }
